fa1467bf65605cdb270f95960af58364519d43bd
Carroll Chiou <cchiou@cloudbees.com>
2020-03-03 15:06:30 -0800

    Update buildThis.groovy

--- vars/buildThis.groovy
+++ vars/buildThis.groovy
@@ -27,9 +27,9 @@ def call() {
                   ) {
                       when {
                           expression {
-                              JENKINS_VERSION = sh returnStdout: true, script: 'mvn help:evaluate -Dexpression=jenkins.version | grep -Eo "^[123456789]*\\..*"'.trim()
-                              if (JENKINS_VERSION < 2.164) {
-                                  error("Current Jenkins version is ${JENKINS_VERSION}. Minimum Jenkins version required for JDK 11 is 2.164.3-cb-2")
+                              def jenkinsVersion = sh returnStdout: true, script: 'mvn help:evaluate -Dexpression=jenkins.version'
+                              if (jenkinsVersion < 2.164) {
+                                  error("Current Jenkins version is ${jenkinsVersion}. Minimum Jenkins version required for JDK 11 is 2.164.3-cb-2")
                               } else {
                                   return true
                               }
@@ -37,8 +37,8 @@ def call() {
                           }
                       }
                       echo 'hey, you passed!'
-                      def JENKINS_VERSION = sh returnStdout: true, script: 'mvn help:evaluate -Dexpression=jenkins.version | grep -Eo "^[123456789]*\\..*"'.trim()
-                      echo "jenkins version: ${JENKINS_VERSION}"
+                      //def JENKINS_VERSION = sh returnStdout: true, script: 'mvn help:evaluate -Dexpression=jenkins.version | grep -Eo "^[123456789]*\\..*"'.trim()
+                      //echo "jenkins version: ${JENKINS_VERSION}"
                     //   sh """
                     //       mvn --show-version --batch-mode --no-transfer-progress --errors \
                     //           -Dconcurrency=1 -Dmaven.test.failure.ignore=true -Dfindbugs.failOnError=false -Djava.level=8\
