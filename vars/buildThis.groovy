4aa6d0def18ce8d0fe02d6dfd3f0e37366206382
Carroll Chiou <cchiou@cloudbees.com>
2020-03-03 15:32:33 -0800

    Update buildThis.groovy

--- vars/buildThis.groovy
+++ vars/buildThis.groovy
@@ -9,51 +9,15 @@ def call() {
         }
 
       stages {
-          stage('JDK 11 - Linux') {
+          stage('testing') {
               agent any
-              //agent {
-                //   label 'docker-dedicated-jdk11 && linux'
-                //label 'maven-11'
-              //}
-
               steps {
-                 // withMaven(
-                    //  maven: 'maven'//,
-                    //   globalMavenSettingsConfig: 'nexus_plus_incrementals',
-                    //   publisherStrategy: 'EXPLICIT',
-                    //   options: [
-                    //       junitPublisher(healthScaleFactor: 1.0),
-                    //       spotbugsPublisher()
-                    //   ]
-                  //) {
-                      /*
-                      when {
-                          expression {
-                              def jenkinsVersion = sh returnStdout: true, script: 'mvn help:evaluate -Dexpression=jenkins.version'
-                              if (jenkinsVersion < 2.164) {
-                                  error("Current Jenkins version is ${jenkinsVersion}. Minimum Jenkins version required for JDK 11 is 2.164.3-cb-2")
-                              } else {
-                                  return true
-                              }
-                              // return JENKINS_VERSION < 2.164
-                          }
-                      }
-                      */
                       script {
                           
                       jenkinsVersion = sh returnStdout: true, script: 'mvn help:evaluate -Dexpression=jenkins.version'
                       echo "${jenkinsVersion}"
                       }
-                      echo 'hey, you passed!'
-                      
-                      //def JENKINS_VERSION = sh returnStdout: true, script: 'mvn help:evaluate -Dexpression=jenkins.version | grep -Eo "^[123456789]*\\..*"'.trim()
-                      //echo "jenkins version: ${JENKINS_VERSION}"
-                    //   sh """
-                    //       mvn --show-version --batch-mode --no-transfer-progress --errors \
-                    //           -Dconcurrency=1 -Dmaven.test.failure.ignore=true -Dfindbugs.failOnError=false -Djava.level=8\
-                    //           clean verify
-                    //   """
-                  }
+
               }
           }
       }
