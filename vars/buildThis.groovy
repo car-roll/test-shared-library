79e3597c8719f42d204a6beba07c7ea3adfe8700
Carroll Chiou <cchiou@cloudbees.com>
2020-03-05 13:54:08 -0800

    Update buildThis.groovy

--- vars/buildThis.groovy
+++ vars/buildThis.groovy
@@ -1,8 +1,5 @@
 #!/usr/bin/env groovy
 def call() {
-    //def minMajor = 2
-    //def minMinor = 164
-    def minVersion = [2, 164, 3]
     pipeline {
         agent none
         options {
@@ -15,22 +12,8 @@ def call() {
               agent any
               steps {
                   script {
-                      jenkinsVersion = sh returnStdout: true, script: 'mvn help:evaluate -Dexpression=jenkins.version -q -DforceStdout'
+                      jenkinsVersion = enforceJenkinsVersion()
                       echo "jenkins version is: ${jenkinsVersion}"
-                      /*
-                      versionSegments = jenkinsVersion.tokenize('.')
-                      if (versionSegments.size() > 2) {
-                          if (versionSegments[0].toInteger() >= minMajor) {
-                              if (versionSegments[1].toInteger() >= minMinor) {
-                                  return true;
-                              }
-                          }
-                      }
-                      */
-                      if (!enforceJenkinsVersion(jenkinsVersion, minVersion)) {
-                          error ("Minimum jenkins version required for JDK11 is: ${minVersion[0]}.${minVersion[1]}.${minVersion[2]}. Current plugin jenkins version is: ${jenkinsVersion}")
-                      }
-                      //error ("Minimum jenkins version required for JDK11 is: ${minMajor}.${minMinor}. Current plugin jenkins version is: ${jenkinsVersion}")
                   }
               }
           }
