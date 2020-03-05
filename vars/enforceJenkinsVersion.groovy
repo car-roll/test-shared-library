35ed73a557c6a5ec43a5e104539cc283c20d5d70
Carroll Chiou <cchiou@cloudbees.com>
2020-03-05 13:51:36 -0800

    Update enforceJenkinsVersion.groovy

--- vars/enforceJenkinsVersion.groovy
+++ vars/enforceJenkinsVersion.groovy
@@ -1,18 +1,19 @@
 #!/usr/bin/env groovy
 
-def call(String jenkinsVersion, List<Integer> minVersion) {
-    echo "jv: ${jenkinsVersion}"
-    echo "mv: ${minVersion}"
+def call() {
+    def minVersionString = "2.164.3-cb-2"
+    def minVersion = [2, 164, 3]
+    def jenkinsVersion = sh returnStdout: true, script: 'mvn help:evaluate -Dexpression=jenkins.version -q -DforceStdout'
     versionSegments = jenkinsVersion.tokenize('.')
     if (versionSegments.size() > 2) {
         rev = versionSegments[2].tokenize('-')[0].toInteger()
         if (versionSegments[0].toInteger() >= minVersion[0]) {
             if (versionSegments[1].toInteger() >= minVersion[1]) {
                 if (rev >= minVersion[2]) {
-                    return true
+                    return jenkinsVersion
                 }
             }
         }
     }
-    return false
+    return minVersionString
 }
