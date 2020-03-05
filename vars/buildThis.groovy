ff0fef4fe63ba790d8d5f37d1edbefdb1861677b
Carroll Chiou <cchiou@cloudbees.com>
2020-03-05 08:59:39 -0800

    Update buildThis.groovy

--- vars/buildThis.groovy
+++ vars/buildThis.groovy
@@ -17,7 +17,7 @@ def call() {
                       jenkinsVersion = sh returnStdout: true, script: 'mvn help:evaluate -Dexpression=jenkins.version -q -DforceStdout'
                       echo "jenkins version is: ${jenkinsVersion}"
                       versionSegments = jenkinsVersion.tokenize('.')
-                      if (versionSegments.size > 2) {
+                      if (versionSegments.size() > 2) {
                           if (versionSegments[0] >= minMajor) {
                               if (versionSegments[1] >= minMinor) {
                                   return true;
