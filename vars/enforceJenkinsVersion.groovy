55b76e115fefa3ed651cfd3b7652ccc162d93737
Carroll Chiou <cchiou@cloudbees.com>
2020-03-05 13:40:11 -0800

    Update enforceJenkinsVersion.groovy

--- vars/enforceJenkinsVersion.groovy
+++ vars/enforceJenkinsVersion.groovy
@@ -12,7 +12,7 @@ def call(String jenkinsVersion, List<Integer> minVersion) {
     if (versionSegments.size() > 2) {
         rev = versionSegments[2].tokenize('-')[0].toInteger()
         if (versionSegments[0].toInteger() >= minVersion[0]) {
-            if (versionSegments[1].toInteger() >= minVersion[2]) {
+            if (versionSegments[1].toInteger() >= minVersion[1]) {
                 if (rev >= minVersion[2]) {
                     return true
                 }
